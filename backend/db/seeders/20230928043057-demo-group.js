57a9f7c8e25284c4ca84db9788d8a72cf3f61cff
Shununit6 <unit6xuli@gmail.com>
2023-09-27 23:19:58 -0700

    set up seeders, finished demo-group,demo-venue

---
+++ backend/db/seeders/20230928043057-demo-group.js
@@ -0,0 +1,50 @@
+'use strict';
+const { Group } = require('../models');
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await Group.bulkCreate([
+      {
+        name: "groupOne",
+        about: "aboutOne",
+        type: "typeOne",
+        private: true,
+        city: "San Jose",
+        state: "CA",
+        previewImage: "previewOne"
+      },
+      {
+        name: "groupTwo",
+        about: "aboutTwo",
+        type: "typeTwo",
+        private: false,
+        city: "San Francisco",
+        state: "CA",
+        previewImage: "previewTwo"
+      },
+      {
+        name: "groupThree",
+        about: "aboutThree",
+        type: "typeThree",
+        private: true,
+        city: "Oakland",
+        state: "CA",
+        previewImage: "previewThree"
+      }
+    ], { validate: true});
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Groups';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      name: {[Op.in]: ['groupOne', 'groupTwo', 'groupThree']}
+    }, {});
+  }
+};
---
+++ backend/db/seeders/20230928052019-demo-venue.js
@@ -0,0 +1,45 @@
+'use strict';
+const { Venue } = require('../models');
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await Venue.bulkCreate([
+      {
+        address: 'Golden Gate Park, 501 Stanyan St',
+        city: 'San Francisco',
+        state: 'CA',
+        lat: 37.769421,
+        lng: -122.486214,
+      },
+      {
+        address: 'Union Square, 333 Post St',
+        city: 'San Francisco',
+        state: 'CA',
+        lat: 37.787979,
+        lng: -122.407516,
+      },
+      {
+        address: 'Fort Mason, 2 Marina Blvd Landmark Building C, Suite 260',
+        city: 'San Francisco',
+        state: 'CA',
+        lat: 37.806786,
+        lng: -122.431419,
+      }
+    ], { validate: true});
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Venues';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      state: {[Op.in]: ['CA']}
+    }, {});
+  }
+};

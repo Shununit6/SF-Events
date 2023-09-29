dda96033ef4d598dbaaf69be7898b0470c495856
Shununit6 <unit6xuli@gmail.com>
2023-09-29 13:47:51 -0700

    basic routes set up

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,25 @@
 const router = require('express').Router();
+const eventimagesRouter = require('./event-images.js');
+const eventsRouter = require('./events.js');
+const groupimagesRouter = require('./group-images.js');
+const groupsRouter = require('./groups.js');
+const venuesRouter = require('./venues.js');
+
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
+
+router.use('/event-images', eventimagesRouter);
+
+router.use('/events', eventsRouter);
+
+router.use('/group-images', groupimagesRouter);
+
+router.use('/groups', groupsRouter);
+
+router.use('/venues', venuesRouter);
 
 router.use('/session', sessionRouter);
 

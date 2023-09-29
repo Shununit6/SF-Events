7937d4baac0c0248d7d86b0a2e7ebd1ec3848f67
Shununit6 <unit6xuli@gmail.com>
2023-09-28 21:04:30 -0700

    Get Current User

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -41,4 +41,20 @@ router.post('/', validateSignup, async (req, res) => {
 }
 );
 
+router.get('/current', (req, res) => {
+    const { user } = req;
+    if (user) {
+        const safeUser = {
+            id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            username: user.username,
+        };
+        return res.json({
+            user: safeUser
+        });
+    } else return res.json({ user: null });
+});
+
 module.exports = router;

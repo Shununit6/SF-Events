212192e79064fd2dfee52a5be52c5e23823ce835
Shununit6 <unit6xuli@gmail.com>
2023-10-01 16:38:34 -0700

    :eventId/images

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
-const { Event, sequelize, User, Group, Venue} = require('../../db/models');
+const { Event, sequelize, User, Group, Venue, EventImage} = require('../../db/models');
+
+const { requireAuth } = require('../../utils/auth');
 
 router.get('/', async (req, res) => {
     const Events = await Event.findAll(
@@ -39,5 +41,29 @@ router.get('/', async (req, res) => {
     );
     return res.json({Events});
 });
+
+// router.post("/:eventId/images", requireAuth, async (req, res, next) => {
+    // console.log(req.params.eventId);
+
+    // const event = await Event.findAll({
+	// 		where: {
+	// 			id: req.params.eventId,
+	// 		},
+	// 	});
+    // if(event.length === 0){
+    //     const err = new Error("Event couldn't be found");
+    //     err.title = "Event couldn't be found";
+    //     err.status = 404;
+    //     return next(err);
+    // }
+    // const { url, preview } = req.body;
+    // const eventimage = await EventImage.create({ eventId, url, preview });
+    // const safeEventImage = {
+    //     id: eventimage.id,
+    //     url: eventimage.url,
+    //     preview: eventimage.preview,
+    // };
+// 	return res.json(event);
+// });
 
 module.exports = router;

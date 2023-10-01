7e12f9ff0d53464e85ada91ff7b5835b6750f56f
Shununit6 <unit6xuli@gmail.com>
2023-10-01 13:51:54 -0700

    getallevent miss numattendees

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -3,31 +3,30 @@ const router = require('express').Router();
 const { Event, sequelize, Group, Venue} = require('../../db/models');
 
 router.get('/', async (req, res) => {
-    const events = await Event.findAll(
+    const Events = await Event.findAll(
         {
-            include: {
-                model: Venue,
-                attributes: {
-                    exclude: ['groupId', 'address', 'lat', 'lng', 'createdAt', 'updatedAt'],
-                    include: ['id', 'city', 'state',]
-                },
-                include: {
+            include: [
+                {
                 model: Group,
                 attributes: {
-                    exclude: ['organizerId','name', 'about', 'type', 'private','previewImage', 'createdAt', 'updatedAt'],
-                    include: ['id', 'city', 'state',]
-                },
-            },
-            },
+                    exclude: ['organizerId', 'about', 'type', 'private','previewImage', 'createdAt', 'updatedAt'],
+                    include: ['id', 'name', 'city', 'state',]
+                },},
+                {
+                    model: Venue,
+                    attributes: {
+                        exclude: ['groupId', 'address', 'lat', 'lng', 'createdAt', 'updatedAt'],
+                        include: ['id', 'city', 'state',]
+                    },},
+            ],
             attributes: {
-                exclude: ['createdAt', 'updatedAt'],
+                exclude: ['description','capacity','price', 'createdAt', 'updatedAt'],
                 include: ['id', 'groupId', 'venueId', 'name', 'type', 'startDate', 'endDate', 'previewImage',]
             },
-            raw: true,
             group: "Event.id",
         }
     );
-    return res.json(events);
+    return res.json({Events});
 });
 
 module.exports = router;

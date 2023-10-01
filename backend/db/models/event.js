9e437b9d5214a6342b41628c5545f559c49dc227
Shununit6 <unit6xuli@gmail.com>
2023-10-01 15:24:33 -0700

    get all events

--- backend/db/models/event.js
+++ backend/db/models/event.js
@@ -19,7 +19,8 @@ module.exports = (sequelize, DataTypes) => {
         {
           through: models.Attendee,
           foreignKey: 'eventId',
-          otherKey: 'userId'
+          otherKey: 'userId',
+          as: "attendees"
         }
       );
     }
--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -1,27 +1,38 @@
 const router = require('express').Router();
 
-const { Event, sequelize, Group, Venue} = require('../../db/models');
+const { Event, sequelize, User, Group, Venue} = require('../../db/models');
 
 router.get('/', async (req, res) => {
     const Events = await Event.findAll(
         {
             include: [
                 {
-                model: Group,
-                attributes: {
-                    exclude: ['organizerId', 'about', 'type', 'private','previewImage', 'createdAt', 'updatedAt'],
-                    include: ['id', 'name', 'city', 'state',]
-                },},
+                    model: Group,
+                    attributes: {
+                        exclude: ['organizerId', 'about', 'type', 'private','previewImage', 'createdAt', 'updatedAt'],
+                        include: ['id', 'name', 'city', 'state',]
+                    },
+                },
                 {
                     model: Venue,
                     attributes: {
                         exclude: ['groupId', 'address', 'lat', 'lng', 'createdAt', 'updatedAt'],
                         include: ['id', 'city', 'state',]
-                    },},
+                    },
+                },
+                {
+                    model: User,
+                    as: "attendees",
+                    attributes: [],
+                    through: {attributes: [],},
+                },
+
             ],
             attributes: {
                 exclude: ['description','capacity','price', 'createdAt', 'updatedAt'],
-                include: ['id', 'groupId', 'venueId', 'name', 'type', 'startDate', 'endDate', 'previewImage',]
+                include: ['id', 'groupId', 'venueId', 'name', 'type', 'startDate', 'endDate', 'previewImage',
+                [sequelize.fn('COUNT', sequelize.col('attendees.id')), 'numAttending']
+                ]
             },
             group: "Event.id",
         }

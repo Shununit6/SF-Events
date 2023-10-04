a72d3c61a96332c8d779be6eaddf9b0f2a006ed5
Shununit6 <unit6xuli@gmail.com>
2023-10-04 12:22:37 -0700

    delete event image

--- backend/routes/api/event-images.js
+++ backend/routes/api/event-images.js
@@ -1,5 +1,36 @@
 const router = require('express').Router();
 
 
+const { Event, EventImage, Group, Membership } = require('../../db/models');
+
+const { requireAuth } = require('../../utils/auth');
+
+router.delete("/:imageId", requireAuth, async (req, res, next) => {
+    const imageId = req.params.imageId;
+    const eventimage = await EventImage.findOne({ where: { id: imageId,	},});
+    if(!eventimage){
+        const err = new Error("Event Image couldn't be found");
+        err.title = "Event Image couldn't be found";
+        err.status = 404;
+        return next(err);
+    }
+    const userId = req.user.id;
+    const groupId = eventimage.groupId;
+    const organizer = await Group.findOne({ where: { id: groupId, organizerId: userId},});
+    const cohost = await Membership.findOne({ where: { groupId: groupId, userId: userId, status: "co-host"}, });
+    if(!organizer && !cohost){
+        const err = new Error("Forbidden");
+        err.status = 403;
+        err.title = 'Require proper authorization';
+        return next(err);
+    }
+    await eventimage.destroy();
+    const deletedEventImage = await EventImage.findOne({ where: {id: imageId,},});
+    if(!deletedEventImage){
+        return res.json({
+            "message": "Successfully deleted"
+          });
+    }
+})
 
 module.exports = router;

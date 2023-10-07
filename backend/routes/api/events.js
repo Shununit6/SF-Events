98a9be7ed26154b69a1828add735f497259535ef
Shununit6 <unit6xuli@gmail.com>
2023-10-07 13:25:44 -0700

    allevent

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -130,13 +130,13 @@ router.get('/', validateQuery, async (req, res) => {
             attributes: {
                 exclude: ['description','capacity','price', 'createdAt', 'updatedAt'],
                 include: ['id', 'groupId', 'venueId', 'name', 'type', 'startDate', 'endDate', 'previewImage',
-                [sequelize.fn('COUNT', sequelize.col('Attendees.id')), 'numAttending'],
-                // [
-				// 	sequelize.literal(
-				// 		`(SELECT COUNT(*) FROM Attendances WHERE Attendances.eventId = \`Event\`.id)`
-				// 	),
-				// 	"numAttending",
-				// ],
+                // [sequelize.fn('COUNT', sequelize.col('Attendees.id')), 'numAttending'],
+                [
+					sequelize.literal(
+						`(SELECT COUNT(*) FROM Attendances WHERE Attendances.eventId = \`Event\`.id)`
+					),
+					"numAttending",
+				],
                 ]
             },
             group: ["Event.id", "Group.id", "Venue.id", "Attendees.id"],

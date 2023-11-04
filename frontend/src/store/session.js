8b67fa9d19986bd045243678a54d59e1062d81c4
Shununit6 <unit6xuli@gmail.com>
2023-11-04 09:52:13 -0700

    set up SignupForm

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -32,10 +32,23 @@ export const login = (user) => async (dispatch) => {
 export const restoreUser = () => async (dispatch) => {
     const response = await csrfFetch("/api/session");
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
     dispatch(setUser(data.user));
     return response;
-}
+};
+
+export const signup = (user) => async (dispatch) => {
+    const {username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+        method: "POST",
+        body: JSON.stringify({
+            username, firstName, lastName, email, password,
+        }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
 
 const initialState = { user: null};
 

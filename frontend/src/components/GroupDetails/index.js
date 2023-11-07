d474f4928183ec3d2ec78915691644ffd7aa6c24
Shununit6 <unit6xuli@gmail.com>
2023-11-07 07:29:14 -0800

    groupDetails

--- frontend/src/components/GroupDetails/index.js
+++ frontend/src/components/GroupDetails/index.js
@@ -1,20 +1,37 @@
-import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux"
+import { loadGroupDetails } from "../../store/groups";
 
 const GroupDetails = () => {
-    const [text, setText ] = useState("");
+    const dispatch = useDispatch();
     const { groupId } = useParams();
+    const groupState = useSelector((state) => state.groups);
+    console.log(groupState);
+    // const groupDetail = groupState[groupId];
+    // const {organizerId, name, about, type, city, state, createdAt, updatedAt} = groupDetail;
+    useEffect(() => {
+        dispatch(loadGroupDetails(groupId))
+    }, [dispatch, groupId])
 
-    const addText = (e) => {
-        e.preventDefault();
-        console.log(text, groupId);
-    };
+    // if (!groupDetail) {
+    //     return <div>Loading...</div>
+    // }
 
-    return(
-        <div>
-
-        </div>
-    );
+    // return(
+    //     <div>
+    //         {/* <h1>{id}</h1> */}
+    //         <h1>{organizerId}</h1>
+    //         <h1>{name}</h1>
+    //         <h1>{about}</h1>
+    //         <h1>{type}</h1>
+    //         {/* <h1>{private}</h1> */}
+    //         <h1>{city}</h1>
+    //         <h1>{state}</h1>
+    //         <h1>{createdAt}</h1>
+    //         <h1>{updatedAt}</h1>
+    //     </div>
+    // );
 };
 
 export default GroupDetails;
--- frontend/src/store/groups.js
+++ frontend/src/store/groups.js
@@ -1,6 +1,7 @@
 // import { csrfFetch } from "./csrf";
 /** Action Type Constants: */
 export const LOAD_GROUPS = "groups/LOAD_GROUPS";
+export const LOAD_GROUP_DETAILS = "groups/LOAD_GROUP_DETAILS";
 // export const GET_GROUP = "groups/GET_GROUP";
 
 /**  Action Creators: */
@@ -9,6 +10,10 @@ export const loadGroups = (groups) => ({
     groups,
 });
 
+export const loadGroupDetails = (groups) => ({
+    type: LOAD_GROUP_DETAILS,
+    groups,
+});
 // export const getGroup = (groups) => ({
 //     type: GET_GROUP,
 //     groups,
@@ -26,6 +31,17 @@ export const getAllGroups = () => async (dispatch) => {
     }
     return res;
 };
+
+export const groupDetails = (groupId) => async dispatch => {
+    const res = await fetch(`/api/groups/${groupId}`)
+
+    if (res.ok) {
+        const data = await res.json();
+        dispatch(loadGroupDetails(data))
+        return data
+    }
+    return res;
+}
 
 // export const getGroupById = (groupId) => async (dispatch) => {
 //     const res = await fetch(`/api/groups/${groupId}`);
@@ -48,7 +64,7 @@ export const getAllGroups = () => async (dispatch) => {
 //     }
 //   };
 
-const groupsReducer = (state = {}, action) => {
+const groupsReducer = (state = { isLoading: true,}, action) => {
     switch (action.type) {
         case LOAD_GROUPS:
             const groupsState = {};
@@ -57,7 +73,13 @@ const groupsReducer = (state = {}, action) => {
                 groupsState[group.id] = group;
             });
             console.log(groupsState);
-            return {...groupsState};
+            return {...groupsState,  isLoading: false,};
+        case LOAD_GROUP_DETAILS: {
+            const groupState = {};
+            console.log("actiongroup",action.groups);
+            groupState[action.groups.id] = action.groups;
+            return {groupState, isLoading: false,};
+        }
         // case GET_GROUP:
         //     const groupState = {};
         //     console.log("getgroupbyid", action.groups);

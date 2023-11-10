6400dbd1c7bf933ca363f6745831bd8f5e940af5
Shununit6 <unit6xuli@gmail.com>
2023-11-09 21:40:55 -0800

    url change

--- backend/db/seeders/20230928052439-demo-event-image.js
+++ backend/db/seeders/20230928052439-demo-event-image.js
@@ -10,17 +10,17 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await EventImage.bulkCreate([
       {
-        url: 'imageurlOne',
+        url: 'https://fastly.picsum.photos/id/305/4928/3264.jpg?hmac=s2FLjeAIyYH0CZl3xuyOShFAtL8yEGiYk31URLDxQCI',
         preview: true,
         eventId: 1
       },
       {
-        url: 'imageurlTwo',
+        url: 'https://fastly.picsum.photos/id/392/5000/3333.jpg?hmac=vCaGuB6rQAiaofdQHatQL4DHgkyR2l-Ms9GWAL63CBQ',
         preview: false,
         eventId: 2
       },
       {
-        url: 'imageurlThree',
+        url: 'https://fastly.picsum.photos/id/431/5000/3334.jpg?hmac=T2rL_gBDyJYpcr1Xm8Kv7L6bhwvmZS8nKT5w3ok58kA',
         preview: true,
         eventId: 3
       }
@@ -31,7 +31,8 @@ module.exports = {
     options.tableName = 'EventImages';
     const Op = Sequelize.Op;
     await queryInterface.bulkDelete(options, {
-      url: {[Op.in]: ['imageurlOne', 'imageurlTwo', 'imageurlThree']}
+      url: {[Op.in]: ['https://fastly.picsum.photos/id/305/4928/3264.jpg?hmac=s2FLjeAIyYH0CZl3xuyOShFAtL8yEGiYk31URLDxQCI',
+      'https://fastly.picsum.photos/id/392/5000/3333.jpg?hmac=vCaGuB6rQAiaofdQHatQL4DHgkyR2l-Ms9GWAL63CBQ', 'https://fastly.picsum.photos/id/431/5000/3334.jpg?hmac=T2rL_gBDyJYpcr1Xm8Kv7L6bhwvmZS8nKT5w3ok58kA']}
     }, {});
   }
 };

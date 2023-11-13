833cef614302b7b3f6c4e031bc9c8f6b2e0bd931
Shununit6 <unit6xuli@gmail.com>
2023-11-12 20:30:47 -0800

    modified events with past/future date

--- backend/db/seeders/20230928052033-demo-event.js
+++ backend/db/seeders/20230928052033-demo-event.js
@@ -18,8 +18,20 @@ module.exports = {
         description: 'descriptionOne',
         capacity: 10,
         price: 18.50,
-        startDate: '2023-12-02 08:00:00',
-        endDate: '2023-12-12 08:00:00',
+        startDate: '2024-11-02 08:00:00',
+        endDate: '2024-11-04 08:00:00',
+        previewImage: 'previewOne'
+      },
+      {
+        groupId: 1,
+        venueId: 1,
+        name: 'eventFour',
+        type: 'Online',
+        description: 'descriptionOne',
+        capacity: 10,
+        price: 18.50,
+        startDate: '2023-11-02 08:00:00',
+        endDate: '2023-11-03 08:00:00',
         previewImage: 'previewOne'
       },
       {
@@ -27,12 +39,24 @@ module.exports = {
         venueId: 2,
         name: 'eventTwo',
         type: 'Online',
-        description: 'descriptionOne',
+        description: 'descriptionTwo',
         capacity: 20,
         price: 10.50,
-        startDate: '2023-11-02 08:00:00',
-        endDate: '2023-12-02 08:00:00',
+        startDate: '2023-11-08 08:00:00',
+        endDate: '2023-11-09 08:00:00',
         previewImage: 'previewTwo'
+      },
+      {
+        groupId: 2,
+        venueId: 2,
+        name: 'eventFive',
+        type: 'Online',
+        description: 'descriptionFive',
+        capacity: 20,
+        price: 10.50,
+        startDate: '2023-11-06 08:00:00',
+        endDate: '2023-11-07 08:00:00',
+        previewImage: 'previewFive'
       },
       {
         groupId: 3,
@@ -42,9 +66,21 @@ module.exports = {
         description: 'descriptionThree',
         capacity: 15,
         price: 5.50,
-        startDate: '2023-12-12 08:00:00',
+        startDate: '2023-12-20 08:00:00',
         endDate: '2023-12-22 08:00:00',
         previewImage: 'previewThree'
+      },
+      {
+        groupId: 3,
+        venueId: 3,
+        name: 'eventSix',
+        type: 'In person',
+        description: 'descriptionSix',
+        capacity: 15,
+        price: 5.50,
+        startDate: '2023-12-12 08:00:00',
+        endDate: '2023-12-14 08:00:00',
+        previewImage: 'previewSix'
       }
     ], { validate: true});
   },
@@ -53,7 +89,7 @@ module.exports = {
     options.tableName = 'Events';
     const Op = Sequelize.Op;
     await queryInterface.bulkDelete(options, {
-      name: {[Op.in]: ['eventOne', 'eventTwo', 'eventThree']}
+      name: {[Op.in]: ['eventOne', 'eventTwo', 'eventThree', 'eventFour','eventFive', 'eventSix']}
     }, {});
   }
 };

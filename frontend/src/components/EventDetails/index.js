e9dea5d628a1f780b150f03c730ef39c292c5638
Shununit6 <unit6xuli@gmail.com>
2023-11-17 23:53:11 -0800

    eventstate

--- frontend/src/components/EventDetails/index.js
+++ frontend/src/components/EventDetails/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams, Link, Redirect } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { eventDetails } from "../../store/events";
+import { groupDetails, getGroupIdEvents} from "../../store/groups";
 import DeleteModal from '../Navigation/DeleteModel';
 import DeleteEventModal from "../DeleteEventModal";
 const EventDetails = () => {
@@ -11,6 +12,7 @@ const EventDetails = () => {
     const [isLoading, setIsLoading] = useState(true);
     const eventData = useSelector((state) => state.events[eventId]);
 
+    // dispatch(groupDetails(groupId)).then(()=>dispatch(getGroupIdEvents(groupId))).then(()=>setIsLoaded(true))
     useEffect(() => {
         dispatch(eventDetails(eventId)).then(()=>setIsLoading(false))
     }, [dispatch, eventId])
@@ -22,21 +24,28 @@ const EventDetails = () => {
     if (isLoading) {
         return (<div>Loading...</div>);
     }
-
+    // const groupData = useSelector((state) => state.groups);
     const {name, startDate, endDate} = eventData;
     // const imageUrl = Object.values(eventDetail.EventImages).find((image) => image.preview === 1).url;
     let imageUrl="";
     if(eventData.EventImages.length > 0){
         imageUrl = eventData.EventImages.find((image) => image.preview == 1).url;
     }
+    let isGroupCreator=false;
+    // console.log("groupData36",groupData);
+    // let organizerId = groupData.organizerId;
+    // if(sessionUser && organizerId === sessionUser.id){
+    //     isGroupCreator=true;
+    // }
     return(
         <div>
             <img id="images" src={imageUrl} alt="event"/>
             <p>name  {name}</p>
             <p>startDate {startDate.slice(0, 10)}</p>
             <p>endDate  {endDate.slice(0, 10)}</p>
+            {/* <p>{groupData}</p> */}
             {/* Action button shows if logged-in user is the creator of the event */}
-            {sessionUser ? <DeleteModal
+            {sessionUser && isGroupCreator ? <DeleteModal
                                 itemText="Delete"
                                 modalComponent={<DeleteEventModal event={eventData}/>}
                                 /> :null}

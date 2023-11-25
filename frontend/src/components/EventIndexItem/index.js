9cca7d40af5796454cba024439afd7feb463a39d
Shununit6 <unit6xuli@gmail.com>
2023-11-24 19:36:28 -0800

    eventlist

--- frontend/src/components/EventIndexItem/index.js
+++ frontend/src/components/EventIndexItem/index.js
@@ -23,6 +23,25 @@ const EventIndexItem = ({ event }) => {
     // console.log(city, state);
     let location = city + ', ' + state;
     // let location = {`${event.Venue.city}`, `${event.Venue.state}`};
+    const pststartDate = new Date(startDate).toLocaleDateString('en-US', {
+        minute: '2-digit',
+        hour:'2-digit',
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        timeZone: "America/Los_Angeles"
+    });
+
+    const formattedPstStartDate = pststartDate.slice(6,10) +"-"+ pststartDate.slice(0,2) + "-" + pststartDate.slice(3,5)
+    +" · "+pststartDate.slice(12)+" PST";
+
+    const totalLaneCount = Math.ceil(description.length/87);
+    let eventDescriptionArr=[];
+    for(let i = 0; i <= totalLaneCount+1; i++){
+        let j = 87;
+        eventDescriptionArr.push(description.slice(j*i, j*i+87)+"\n");
+        j += 87;
+    };
 
     let imageUrl="";
     if(eventData[event.id].EventImages.length > 0){
@@ -39,12 +58,12 @@ const EventIndexItem = ({ event }) => {
                         <img id = "eventImage" src={imageUrl} alt="event"/>
                     </div>
                     <div id="item10">
-                        <p>{startDate.slice(0,10)}</p>
+                        <p>{formattedPstStartDate}</p>
                         <h1>{name}</h1>
                         <p>{location}</p>
                     </div>
                     <div id="item11">
-                        <p>{description}</p>
+                        <p>{eventDescriptionArr}</p>
                     </div>
                 </div>
                 </Link>

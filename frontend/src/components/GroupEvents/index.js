cf6299b1d2512d7457f72d851d40dbc8daaefb6e
Shununit6 <unit6xuli@gmail.com>
2023-11-24 16:15:23 -0800

    startDate

--- frontend/src/components/GroupEvents/index.js
+++ frontend/src/components/GroupEvents/index.js
@@ -19,11 +19,22 @@ const GroupEvents = ({ event }) => {
     if(!isLoaded) {
         return (<div>Loading...</div>);
     }
-    const {name, id, startDate, Venue} = event;
+    const {name, id, startDate, Venue, endDate} = event;
     let city = Venue.city;
     let state = Venue.state;
     // console.log(city, state);
     let location = city + ', ' + state;
+    let startTime = startDate.slice(11,13);
+    // if(parseInt(startTime) > 12){
+    //     startTime=parseInt(startTime)-12+startDate.slice(13,16)+"pm";
+    // }else{
+    //     startTime=startDate.slice(11,16)+"am";
+    // }
+    const mystartDate = new Date(startDate);
+    const pststartDate = mystartDate.toLocaleString("en-US", {
+    timeZone: "America/Los_Angeles"
+    });
+    console.log(pststartDate);
 
     let imageUrl="";
     if(eventData[event.id].EventImages.length > 0){
@@ -39,7 +50,11 @@ const GroupEvents = ({ event }) => {
                         <img id = "eventImage" src={imageUrl} alt="event"/>
                     </div>
                     <div id="item7">
-                        <p>{startDate.slice(0,10)}</p>
+                        {/* <p>{startDate.slice(0,10)}{" · "}{parseInt(startTime)-8}{startDate.slice(11,13)}{startDate.slice(13,16)}</p> */}
+                         {/* <p>{endDate.slice(0,10)}{" · "}{endDate.slice(11,13)}{endDate.slice(13,16)}</p> */}
+                        <p>{pststartDate}</p>
+                        <p>{startDate.toString()}</p>
+                        <p>{endDate.toString()}</p>
                         <h1>{name}</h1>
                         <p>{location}</p>
                     </div>
